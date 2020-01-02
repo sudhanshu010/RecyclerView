318d5a411cd920023ecd594426ada009c549a930
vikas <goyalvikas546@gmail.com>
2020-01-02 15:54:48 +0530

    committed

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
         itemList = new ArrayList<Item>();
         sjlkvjsdlkfjsdlkfj;
+        hfghbdnkf;
         System.out.println("heyy");
         Item item1 = new Item("sudhanshu");
         Item item2 = new Item("sudhanshu");
