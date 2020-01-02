7f96d11b95f092949c62724e84a72b9d8dd433ab
Sudhanshu Gupta <sudhanshu10101999@gmail.com>
2020-01-02 15:44:09 +0530

    heyy

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -22,8 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recyclerView);
         itemList = new ArrayList<Item>();
-
-
+        sjlkvjsdlkfjsdlkfj;
         System.out.println("heyy");
         Item item1 = new Item("sudhanshu");
         Item item2 = new Item("sudhanshu");
